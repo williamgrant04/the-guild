1aa4db39a4db8d6da769b3c725c7293c59deac01
Edgard Revol <edgardrevolr@gmail.com>
2024-05-07 12:19:43 +0200

    added the scope for the member_game_name uniqueness

--- app/models/game_member.rb
+++ app/models/game_member.rb
@@ -4,7 +4,7 @@ class GameMember < ApplicationRecord
   belongs_to :game
   # validations
   validates :details, :member_game_name, presence: true
-  validates :member_game_name, uniqueness: { case_sensitive: true }
+  validates :member_game_name, uniqueness: { case_sensitive: true, scope: game_id }
   validates :details, length: { maximum: 200, too_long: "%<count>s characters is the maximum allowed" }
   validates :member_game_name, length: { minimum: 3, too_short: "%<count>s characters is the minimum allowed" }
 end
