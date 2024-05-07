0e2802ad4e5169a7ad9849c63fce7975e2f2a549
Brad Dougherty <brad.doughertys@gmail.com>
2024-05-07 17:06:58 +0200

    reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: "users/sessions"
   }
-  resources :events, only: %i[new create edit update show]
   resources :guilds, except: %i[destroy index]
+  resources :events, only: %i[new create edit update show]
   # *Modals
   # For the modals, write custom paths following the below pattern
   # get "modals/test", to: "modals#test"
