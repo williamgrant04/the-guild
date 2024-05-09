46c19787499815226a9726cd1f90bc3e5fabaa04
William Grant <williamg3575467@gmail.com>
2024-05-09 14:19:01 +0100

    Update invite link system

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static targets = [ 'source' ]
-
-  connect() {
-  }
+  static values = { code: String }
 
   copy () {
-    navigator.clipboard.writeText(this.sourceTarget.value)
+    navigator.clipboard.writeText(`${window.location.origin}/invite/${this.codeValue}`)
+
+    this.element.innerText = "Copied!"
+
+    setTimeout(() => {
+      this.element.innerText = "Invite a member"
+    }, 2000)
   }
 }
