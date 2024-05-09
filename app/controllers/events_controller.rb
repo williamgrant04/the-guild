f67f39bc33c33baf049372de8ba836ecf0b5e37b
Brad Dougherty <brad.doughertys@gmail.com>
2024-05-09 16:26:59 +0200

    Styled the calendar and set up events index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,36 @@
 class EventsController < ApplicationController
+  before_action :set_source
+
+  def index
+    @events = policy_scope(Event)
+    # This finds all events for the source (game or guild)
+    @all_events = @source.events
+    # If the source is an instance of Guild we only find events that are not associated with a game
+    @source.is_a?(Guild) ? @all_events = @all_events.where(game: nil) : @all_events
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
   def new
     @event = Event.new
     @all_events = Event.all
+    authorize @event
   end
 
   def create
     @event = Event.new(event_params)
     # This sets the guild or game depending on what is in the params
-    @event.game = params[:game_id] if params[:game_id].present?
-    @even.guild = params[:guild_id] if params[:guild_id].present?
+    @event.game = Game.find(params[:game_id]) if params[:game_id].present?
+    @event.guild = Guild.find(params[:guild_id]) if params[:guild_id].present?
     if @event.save
       redirect_to root_path, notice: 'Event created successfully'
     else
       render :new, status: :unprocessable_entity
     end
+    authorize @event
   end
 
   def edit
@@ -35,5 +52,13 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:name, :start, :end, :description)
+  end
+
+  def set_source
+    if params[:game_id]
+      @source = Game.find(params[:game_id])
+    elsif params[:guild_id]
+      @source = Guild.find(params[:guild_id])
+    end
   end
 end
---
+++ app/policies/event_policy.rb
@@ -0,0 +1,29 @@
+class EventPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    user.member.guild == record.guild
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    # Only the owner of the guild can create an event
+    user.member.role == 'admin'
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    # Only the owner of the guild can edit the event
+  end
+end
