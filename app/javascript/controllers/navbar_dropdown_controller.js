a4b35737a0aad7a295444b7364da8d61fce4bec1
Edgard Revol <edgardrevolr@gmail.com>
2024-05-10 12:57:00 +0200

    fixed the navbar dropdown

---
+++ app/javascript/controllers/navbar_dropdown_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar-dropdown"
+export default class extends Controller {
+  static targets = ["dropdown"]
+
+  toggleDropdown() {
+    this.dropdownTarget.classList.toggle("d-none")
+  }
+}
