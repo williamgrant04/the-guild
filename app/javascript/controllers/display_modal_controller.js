b50b237793ea1b98abbae75622ebd967375cb46f
William Grant <williamg3575467@gmail.com>
2024-05-10 11:54:12 +0100

    Edit some modal logic and restyle some parts of sidebar

--- app/javascript/controllers/display_modal_controller.js
+++ app/javascript/controllers/display_modal_controller.js
@@ -2,10 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display-modal"
 export default class extends Controller {
-  static targets = [ "root", "data" ]
+  static targets = [ "root" ]
 
-  show() {
-    fetch(`${window.location.origin}/modals/${this.dataTarget.dataset.modal}`)
+  show(e) {
+    fetch(`${window.location.origin}/modals/${e.currentTarget.dataset.modal}`)
       .then(res => res.text())
       .then((html) => {
         this.rootTarget.innerHTML = html
@@ -14,7 +14,6 @@ export default class extends Controller {
   }
 
   hide(e) {
-    console.log(e);
     if (e.target === this.rootTarget.querySelector(".modal-wrapper") || e.type === "keyup") {
       this.rootTarget.innerHTML = ""
     }
