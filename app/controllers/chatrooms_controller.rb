9315e6df180bec89eb234fbd1697fec0f946b758
William Grant <williamg3575467@gmail.com>
2024-05-13 13:54:23 +0100

    Link issue fix (was directing to guild show instead of game show)

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,7 +2,6 @@ class ChatroomsController < ApplicationController
   before_action :set_source
 
   def show
-    @chatroom = Chatroom.find(params[:id])
     @guild = @chatroom.guild || @chatroom.game.guild
     @messages = @chatroom.messages.includes(:member).order(created_at: :asc).limit(100).reverse
     @message = Message.new
@@ -12,10 +11,11 @@ class ChatroomsController < ApplicationController
   private
 
   def set_source
-    if params[:game_id]
-      @source = Game.find(params[:game_id])
-    elsif params[:guild_id]
-      @source = Guild.find(params[:guild_id])
+    @chatroom = Chatroom.find(params[:id])
+    if @chatroom.guild.nil?
+      @source = @chatroom.game || Game.find(params[:game_id])
+    else
+      @source = @chatroom.guild || Guild.find(params[:guild_id])
     end
   end
 end
