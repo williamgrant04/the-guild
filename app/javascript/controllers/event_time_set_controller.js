c03e415efe92c7d93131522e41a3e1553b71e3a6
Edgard Revol <edgardrevolr@gmail.com>
2024-05-13 15:54:48 +0200

    changed the date format from the input in event#new

--- app/javascript/controllers/event_time_set_controller.js
+++ app/javascript/controllers/event_time_set_controller.js
@@ -4,6 +4,6 @@ import flatpickr from "flatpickr"
 // Connects to data-controller="event-time-set"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element, {enableTime: true})
+    flatpickr(this.element, {enableTime: true, dateFormat: "d/m/Y h:m "})
   }
 }
