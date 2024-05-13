6c564b4b20c21411eb77d92fcf2902ef0663202d
William Grant <williamg3575467@gmail.com>
2024-05-13 09:53:53 +0100

    Add the javascript functionality for sending and recieving messages, and refactor message to partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,15 @@ class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
-    authorize @message
     @message.chatroom = @chatroom
     @message.member = current_user.member
+    authorize @message
     if @message.save
-      redirect_to chatroom_path(@chatroom)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "shared/message", locals: {message: @message})
+      )
+      head :ok
     else
       # This sucks ass for this but it works for now, not going to spend 2 hours making it look better, no time
       render "chatrooms/show", status: :unprocessable_entity
